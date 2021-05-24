62463b02afa8f3cad19695fb9a53bda84551d309
unknown <donglei@hollysys.net>
2021-05-24 20:04:39 +0800

    Inser Pdf Encrypt Project

---
+++ PDFEncyption/PDFEncyption/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace PDFEncyption
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SrcPathBtn = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.DesEncrypPathText = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.OpenFilPassWText = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.PerrmisionPassText = new System.Windows.Forms.TextBox();
+            this.EncryptionBtn = new System.Windows.Forms.Button();
+            this.DesBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(52, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "源文件：";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(148, 55);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(428, 21);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "请选择待加密PDF文件";
+            // 
+            // SrcPathBtn
+            // 
+            this.SrcPathBtn.Location = new System.Drawing.Point(604, 53);
+            this.SrcPathBtn.Name = "SrcPathBtn";
+            this.SrcPathBtn.Size = new System.Drawing.Size(75, 23);
+            this.SrcPathBtn.TabIndex = 2;
+            this.SrcPathBtn.Text = "选择文件";
+            this.SrcPathBtn.UseVisualStyleBackColor = true;
+            this.SrcPathBtn.Click += new System.EventHandler(this.SrcPathBtn_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(45, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "加密文件路径：\r\n";
+            // 
+            // DesEncrypPathText
+            // 
+            this.DesEncrypPathText.Enabled = false;
+            this.DesEncrypPathText.Location = new System.Drawing.Point(148, 88);
+            this.DesEncrypPathText.Name = "DesEncrypPathText";
+            this.DesEncrypPathText.Size = new System.Drawing.Size(428, 21);
+            this.DesEncrypPathText.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(46, 135);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "文件打开密码：";
+            // 
+            // OpenFilPassWText
+            // 
+            this.OpenFilPassWText.Location = new System.Drawing.Point(148, 128);
+            this.OpenFilPassWText.Name = "OpenFilPassWText";
+            this.OpenFilPassWText.Size = new System.Drawing.Size(200, 21);
+            this.OpenFilPassWText.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(45, 169);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 12);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "权限更改密码：";
+            // 
+            // PerrmisionPassText
+            // 
+            this.PerrmisionPassText.Location = new System.Drawing.Point(148, 164);
+            this.PerrmisionPassText.Name = "PerrmisionPassText";
+            this.PerrmisionPassText.Size = new System.Drawing.Size(200, 21);
+            this.PerrmisionPassText.TabIndex = 9;
+            // 
+            // EncryptionBtn
+            // 
+            this.EncryptionBtn.Location = new System.Drawing.Point(47, 213);
+            this.EncryptionBtn.Name = "EncryptionBtn";
+            this.EncryptionBtn.Size = new System.Drawing.Size(75, 23);
+            this.EncryptionBtn.TabIndex = 10;
+            this.EncryptionBtn.Text = "文档加密";
+            this.EncryptionBtn.UseVisualStyleBackColor = true;
+            this.EncryptionBtn.Click += new System.EventHandler(this.EncryptionBtn_Click);
+            // 
+            // DesBtn
+            // 
+            this.DesBtn.Location = new System.Drawing.Point(604, 90);
+            this.DesBtn.Name = "DesBtn";
+            this.DesBtn.Size = new System.Drawing.Size(75, 23);
+            this.DesBtn.TabIndex = 11;
+            this.DesBtn.Text = "目标路径";
+            this.DesBtn.UseVisualStyleBackColor = true;
+            this.DesBtn.Click += new System.EventHandler(this.DesBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(695, 283);
+            this.Controls.Add(this.DesBtn);
+            this.Controls.Add(this.EncryptionBtn);
+            this.Controls.Add(this.PerrmisionPassText);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.OpenFilPassWText);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.DesEncrypPathText);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.SrcPathBtn);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "PDFEncryption";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button SrcPathBtn;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox DesEncrypPathText;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox OpenFilPassWText;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox PerrmisionPassText;
+        private System.Windows.Forms.Button EncryptionBtn;
+        private System.Windows.Forms.Button DesBtn;
+    }
+}
+
---
+++ PDFEncyption/PDFEncyption/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using iTextSharp.text.pdf;
+namespace PDFEncyption
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void EncryptionBtn_Click(object sender, EventArgs e)
+        {
+            String scourcePath = this.textBox1.Text;
+            String destinationPath = this.DesEncrypPathText.Text;
+            destinationPath += "\\Encry.pdf";
+            String strOpenPassword = this.OpenFilPassWText.Text;
+            String strOWnerPassword = this.PerrmisionPassText.Text;
+            bool bRet=EncryptPDF(scourcePath,destinationPath, strOpenPassword, strOWnerPassword);
+            if (bRet)
+                MessageBox.Show(destinationPath+":加密成功"+"___生成文件路径："+ destinationPath, "EncryPDF");
+            else
+                MessageBox.Show(destinationPath + ":加密失败", "EncryPDF");
+        }
+
+        private void SrcPathBtn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Filter = "文本文件|*.pdf*";//|C#文件|*.cs|所有文件|*.*
+            if (DialogResult.OK == fileDialog.ShowDialog())
+            {
+                this.textBox1.Text = fileDialog.FileName;
+            }
+            
+        }
+
+        private void DesBtn_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.Description = "请选择文件路径";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                this.DesEncrypPathText.Text = dialog.SelectedPath;
+            }
+        }
+
+
+        private bool EncryptPDF(String strPath,String StrDesPath,String strOpenPassWord,String strOwnerPassWord)
+        {
+            try
+            {
+                PdfReader itextPdfReader = new PdfReader(strPath);
+                iTextSharp.text.Document document = new iTextSharp.text.Document();
+                FileStream fileStream = new FileStream(StrDesPath, FileMode.Create);
+                PdfWriter pdfWriter = PdfWriter.GetInstance(document, fileStream);
+                String strOpenPas, strOwnerPas;
+                strOpenPas = this.OpenFilPassWText.Text;
+                strOwnerPas = this.PerrmisionPassText.Text;
+                pdfWriter.SetEncryption(PdfWriter.STRENGTH40BITS, strOpenPas, strOwnerPas, PdfWriter.AllowCopy);
+                document.Open();
+
+                //Setting PDF file enctyption page count 
+                document.SetPageSize(iTextSharp.text.PageSize.LEDGER.Rotate());
+                iTextSharp.text.pdf.PdfContentByte oDirectContent = pdfWriter.DirectContent;
+                int iNUmberOfPages = itextPdfReader.NumberOfPages;
+                int iPage = 0;
+
+                while (iPage < iNUmberOfPages)
+                {
+                    iPage += 1;
+                    document.SetPageSize(itextPdfReader.GetPageSizeWithRotation(iPage));
+                    document.NewPage();
+                    iTextSharp.text.pdf.PdfImportedPage oPdfImportedPage = pdfWriter.GetImportedPage(itextPdfReader, iPage);
+                    int iRotation = itextPdfReader.GetPageRotation(iPage);
+                    if (iRotation == 90 || iRotation == 270)
+                    {
+                        oDirectContent.AddTemplate(oPdfImportedPage, 0, -1.0F, 1.0F, 0, 0, itextPdfReader.GetPageSizeWithRotation(iPage).Height);
+                    }
+                    else
+                    {
+                        oDirectContent.AddTemplate(oPdfImportedPage, 1.0F, 0, 0, 1.0F, 0, 0);
+                    }
+
+                }
+                document.Close();
+                return true;
+            }catch(Exception e)
+            {
+                //e.ToString();
+                
+                return false;
+            }
+           
+        }
+
+
+    }
+}
